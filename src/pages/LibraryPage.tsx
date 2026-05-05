1a787a47b266854bdc99b4fe49380f9689bf448f
JulietaWeber <juliweber1302@gmail.com>
2026-05-05 14:12:54 -0300

    add: Añadi 3 archivos principales y 1 de componentes

---
+++ src/pages/LibraryPage.tsx
@@ -0,0 +1,14 @@
+
+
+const LibraryPage: React.FC<{}> = ({
+
+}) => {
+  return (
+    <div>
+      <h1>Library</h1>
+      <p>Welcome to the Library page!</p>
+    </div>
+  );
+};
+
+export default LibraryPage;
