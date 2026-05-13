641e1fc046c211ce06aa5bf08ca586605faf6e57
loucimf <facundo.loucim@gmail.com>
2026-05-13 11:26:37 -0300

    update: re-trigger vercel-deploy

--- frontend/src/api/global.api.ts
+++ frontend/src/api/global.api.ts
@@ -1,7 +1,7 @@
 
 export const API_URLS = {
     DEV: "http://localhost:3001/api",
-    VERCEL_PROD: "url",
+    VERCEL_PROD: "vercel_prod_back_url",
     RAWG: "url",
     CHEAPSHARK: "url",
 } as const;
