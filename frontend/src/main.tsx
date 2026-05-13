896becac2e8f97a18dbeadeeefad675a5e5c79a5
loucimf <facundo.loucim@gmail.com>
2026-05-12 21:19:48 -0300

    fix: fixing this bigass mess (copy old src into new frontend)

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css";
+import "./styles/tokens/variables.css";
+import "./styles/base/app.css";
+import "./styles/components/forms.css";
+import "./styles/components/buttons.css";
+import "./styles/components/auth.css";
+import "./styles/components/sidebar.css";
+import "./styles/utilities/layout.css";
+import "./styles/utilities/size.css";
+import "./styles/utilities/spacing.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
